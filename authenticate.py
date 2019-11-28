162241086375c8607c38f179faeee67fe1bd2358
AnonymDavid <jodavid512@gmail.com>
2019-11-28 18:19:01 +0100

    Added LED+functions

--- authenticate.py
+++ authenticate.py
@@ -1,11 +1,13 @@
+#!/usr/bin/env python
+
 # mail + general
-import poplib
+import smtplib
 import string, random
 import StringIO, rfc822
 import logging
 import time
 
-time.sleep(5)
+time.sleep(3)
 
 # authentication
 import pyotp
@@ -18,93 +20,168 @@ SERVER = "pop.gmail.com"
 USER  = "adauthentication@gmail.com"
 PASSWORD = "password"
 
+RELAY=21
+LED_G=14
+LED_R=18
+
+green=False # keep track of led color
+
 # initialize GPIO pin
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(21,GPIO.OUT,initial=GPIO.LOW)
+GPIO.setup(RELAY,GPIO.OUT,initial=GPIO.LOW) # Relay
+GPIO.setup(LED_G,GPIO.OUT,initial=GPIO.LOW) # green led
+GPIO.setup(LED_R,GPIO.OUT,initial=GPIO.LOW) # red led
 
-print("start")
+# turn on GPIO pin
+def gON(g_num):
+	GPIO.output(g_num,GPIO.HIGH)
+
+# turn off GPIO pin
+def gOFF(g_num):
+	GPIO.output(g_num,GPIO.LOW)
+
+closed = True
+
+#print("start")
 reader = SimpleMFRC522()
 
+gON(LED_R)
+time.sleep(1)
+gOFF(LED_R)
+gON(LED_G)
+time.sleep(1)
+gOFF(LED_G)
+
 try:
-	# waiting for RFID read
-	id, text = reader.read()
-	
-	if(id==39583897861 or id==584189945031):
-		print("OK")
-		
-		# connecting to server
-		server = poplib.POP3_SSL(SERVER)
-
-		# login
-		server.user(USER)
-		server.pass_(PASSWORD)
-
-		# list items on server
-		resp, items, octets = server.list()
-
-		# download the first message in the list
-		id, size = string.split(items[len(items)-1])
-		resp, text, octets = server.retr(id)
-
-		# convert list to Message object
-		text = string.join(text, "\n")
-		file = StringIO.StringIO(text)
-		message = rfc822.Message(file)
-
-		lastEmailReceived=message['Date']
-		newEmail=False
-
-		print("initialized")
-
-		timeLimit=25
-		initialTime=time.time()
-
-		while newEmail == False and time.time()-initialTime < timeLimit:
-			print(time.time()-initialTime)
-			# connect to server
-			server = poplib.POP3_SSL(SERVER)
+	while (True):
+		if (closed):
+			# waiting for RFID read
+			id, text = reader.read()
 			
-			# login
-			server.user(USER)
-			server.pass_(PASSWORD)
-			
-			# list items on server
-			resp, items, octets = server.list()
-			
-			# download the first message in the list
-			id, size = string.split(items[len(items)-1])
-			resp, text, octets = server.retr(id)
-			
-			# convert list to Message object
-			text = string.join(text, "\n")
-			file = StringIO.StringIO(text)
-			message = rfc822.Message(file)
-			
-			# check for new email
-			if message['Date'] != lastEmailReceived:
-				newEmail = True
+			if(id==39583897861 or id==584189945031):
+				# correct RFID
+				#print("OK")
+				
+				gON(LED_G)
+				
+				# connecting to server
+				popserver = poplib.POP3_SSL(SERVER)
 
-		# get 2FA password
-		authPassword=pyotp.TOTP("UAFXHLEV3PJH3BUG").now()
-		
-		# compare received message with password
-		if time.time()-initialTime >= timeLimit:
-			# time exceeded
-			print("Too late :/")
-		elif authPassword == message["Subject"]:
-			# correct password
-			print("Correct ", message["Subject"], " = ", authPassword)
-			
-			# open relay for 3 sec
-			GPIO.output(21,GPIO.HIGH)
-			time.sleep(3)
-			GPIO.output(21,GPIO.LOW)
+				# login
+				popserver.user(USER)
+				popserver.pass_(PASSWORD)
+
+				# list items on popserver
+				resp, items, octets = popserver.list()
+
+				# download the first message in the list
+				id, size = string.split(items[len(items)-1])
+				resp, text, octets = popserver.retr(id)
+
+				# convert list to Message object
+				text = string.join(text, "\n")
+				file = StringIO.StringIO(text)
+				message = rfc822.Message(file)
+
+				lastEmailReceived=message['Date']
+				newEmail=False
+
+				#print("initialized")
+				
+				gOFF(LED_G)
+				
+				timeLimit=25
+				initialTime=time.time()
+
+				while newEmail == False and time.time()-initialTime < timeLimit:
+					#print(time.time()-initialTime)
+					
+					# connect to server
+					popserver = poplib.POP3_SSL(SERVER)
+					
+					# login
+					popserver.user(USER)
+					popserver.pass_(PASSWORD)
+					
+					# list items on popserver
+					resp, items, octets = popserver.list()
+					
+					# download the first message in the list
+					id, size = string.split(items[len(items)-1])
+					resp, text, octets = popserver.retr(id)
+					
+					# convert list to Message object
+					text = string.join(text, "\n")
+					file = StringIO.StringIO(text)
+					message = rfc822.Message(file)
+					
+					# check for new email
+					if message['Date'] != lastEmailReceived:
+						newEmail = True
+
+				# get 2FA password
+				authPassword=pyotp.TOTP("UAFXHLEV3PJH3BUG").now()
+				
+				# compare received message with password
+				if time.time()-initialTime >= timeLimit:
+					# time exceeded
+					gON(LED_R)
+					time.sleep(2)
+					gOFF(LED_R)
+				elif authPassword == message["Subject"]:
+					# correct password
+					gON(LED_G)
+					
+					# open relay for 3 sec
+					gON(RELAY)
+					time.sleep(3)
+					gOFF(RELAY)
+					
+					gOFF(LED_G)
+					closed = False
+				else:
+					# incorrect password
+					gON(LED_R)
+					time.sleep(2)
+					gOFF(LED_R)
+			elif (id == 584195779439):
+				# special, exit program card
+				gON(LED_G)
+				time.sleep(1)
+				gOFF(LED_G)
+				gON(LED_R)
+				time.sleep(1)
+				gOFF(LED_R)
+				
+				exit()
+			else:
+				# Wrong RFID
+				gON(LED_R)
+				time.sleep(2)
+				gOFF(LED_R)
 		else:
-			# incorrect password
-			print("Incorrect ", message["Subject"], " != ", authPassword)
-	else:
-		print("Nah")
+			# waiting for RFID read
+			id, text = reader.read()
+			
+			if(id == 39583897861 or id == 584189945031):
+				# correct RFID
+				gON(LED_G)
+				
+				gON(RELAY)
+				time.sleep(3)
+				gOFF(RELAY)
+				
+				gOFF(LED_G)
+				closed = True
+			elif (id == 584195779439):
+				exit()
+			else:
+				# wrong RFID
+				gON(LED_R)
+				time.sleep(2)
+				gOFF(LED_G)
+except:
+	print("something went wrong")
 finally:
 	GPIO.cleanup()
-
