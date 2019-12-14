fdc74f1757a9fe0c90eaad329d54ba6c9eaa7e52
haliscsharp <haliscsharp@gmail.com>
2019-12-14 16:02:04 +0100

    Database creation

---
+++ createDatabase.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+conn = sqlite3.connect('rfid_box_db.sqlite')
+
+cur = conn.cursor()
+
+cur.execute('CREATE TABLE BoxOpenings (CardID VARCHAR, Time TIMESTAMP, OpenInterval INTEGER, PRIMARY KEY (Time))')
+
+conn.commit()
+
+conn.close()
+
